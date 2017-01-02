2f2aef2e271bfc2fb34a2b6b107439edd2059a79
Dominic Farquharson <farquharsondominic@gmail.com>
2017-01-02 16:00:51 -0500

    Creating App Layout w/ Components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 //importing render method
 import { render } from 'react-dom';
 import './css/style.css';
+import App from './components/App';
 import StorePicker from './components/StorePicker';
 
 render(
   //rendering StorePicker component
-  <StorePicker />,
+  <App />,
   //selecting place component is being rendered to
   document.querySelector('#main')
 );
