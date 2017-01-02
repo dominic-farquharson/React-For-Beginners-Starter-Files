9f5020d1a704e966951c28caaa9716c537d68cd1
Dominic Farquharson <farquharsondominic@gmail.com>
2017-01-02 15:17:40 -0500

    Creating StorePicker Component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+//needed for every component
+import React from 'react';
+
+//creating a StorePicker component
+class StorePicker extends React.Component {
+  //every component needs Render method
+  render() {
+    return <p>Hello</p>
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,12 @@
-// let's go!
+import React from 'react';
+//importing render method
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+render(
+  //rendering StorePicker component
+  <StorePicker />,
+  //selecting place component is being rendered to
+  document.querySelector('#main')
+);
